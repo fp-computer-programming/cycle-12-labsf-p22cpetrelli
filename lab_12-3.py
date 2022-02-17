c20bd4fa6cc083b2c240d33d07cc3043b63a3b28
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2022-02-17 09:42:31 -0500

    add file

---
+++ lab_12-3.py
@@ -0,0 +1,8 @@
+#author CJP 2/17/22
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+weekend = ("Saturday", "Sunday")
+
+days_of_the_week = weekdays + weekend
+
+print(days_of_the_week)
